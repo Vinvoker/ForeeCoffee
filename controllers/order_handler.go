e0f1d10204f83e21fe187d4cb3107de303145f69
michaelasheren <sherenmichaela@gmail.com>
2023-04-22 12:05:44 +0700

    endpoint order history

--- controllers/order_handler.go
+++ controllers/order_handler.go
@@ -1,9 +1,25 @@
 package controllers
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func InsertOrder(c *gin.Context) {
+	db := connect()
+	defer db.Close()
+
+	var newOrder Order
+
+	if errBind := c.Bind(&newOrder); errBind != nil {
+		fmt.Print(errBind)
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+}
+
+func HistoryOrder(c *gin.Context) {
 
 }
